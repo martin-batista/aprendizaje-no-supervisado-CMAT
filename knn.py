5a21278368e67f7a102dbfaa1bc2a46eab7356a1
= <=>
2021-08-28 17:31:17 -0300

    Cleaned up knn.

--- knn.py
+++ knn.py
@@ -38,80 +38,14 @@ if __name__ == '__main__':
     kmeans = KMeans(n_clusters=28)
     kmeans.fit(data)
 
-    plot_clusters(data, kmeans, True)
+    plot_clusters(data, kmeans)
 
 # %%
 gt = GraphTools(data)
 gt.plot_all()
-# gt.plot_delaunay()
-# gt.plot_gabriel()
+gt.plot_delaunay()
+gt.plot_gabriel()
 # gt.plot_all()
-
-# %%
-from itertools import combinations
-from scipy.spatial import Delaunay
-from time import perf_counter
-from numba import jit
-import math
-
-points = make_data(n=300)
-# points = np.array([[1,2], [3,5], [7,3], [1,5], [4,2]])
-make_plot()
-plot_data(points)
-t1 = perf_counter()
-tri = Delaunay(points)
-t2 = perf_counter()
-print('Delaunay', t2-t1)
-plt.triplot(points[:,0], points[:,1], tri.simplices, color='hotpink')
-
-
-def inside_circle(x : np.ndarray, y : np.ndarray , point : np.ndarray) -> bool:
-    """Evalutes whether or not point is inside the circle of diameter xy."""
-    # diam = l2_squared_distance(x,y)
-    # return diam**2 > l2_squared_distance(x,point) ** 2 + l2_squared_distance(y,point) **2
-    diam_sq = np.sum(np.power(x-y, 2))
-    return diam_sq > np.sum(np.power(x-point,2)) + np.sum(np.power(y-point,2)) 
-
-def gabriel(simplices):
-    removed_edges = [] 
-    edges = []
-    simplices = tri.simplices
-    for simplex in simplices: #Iterate over simplices.
-        for edge in combinations(simplex, 2): #Iterate over edges in the simplex.
-            if edge not in removed_edges:
-                edges.append(frozenset(edge))
-                edge_point_x, edge_point_y = points[edge[0]], points[edge[1]]
-                for neighbor in set(simplex).difference(set(edge)):
-                    neighbor_point = points[neighbor]
-                    if inside_circle(edge_point_x, edge_point_y, neighbor_point):
-                        removed_edges.append(frozenset(edge))
-                        
-    # removed_edges = [tuple(removed_edge) for removed_edge in removed_edges]
-    edges = set(edges).difference(set(removed_edges))
-    # edges = np.array(list(edges))
-    return edges
-
-t1 = perf_counter()
-edges = gabriel(tri.simplices)
-t2 = perf_counter()
-
-print('Gabriel', t2-t1)
-# edges_ = np.array([list(edge) for edge in edges])
-# make_plot()
-# plot_edges = np.array([list(list(edge)) for edge in edges])
-for edge in edges:
-    plt.plot(points[[*edge]][:,0], points[[*edge]][:,1], '-', color='palegreen')
-    # plt.plot(points[edge][:,0], points[edge][:,1], '-', color='palegreen')
-    # plt.plot(points[[*edge]][:,0], points[[*edge]][:,1], '-', color='hotpink')
-    plot_data(points[[*edge]], size=40)
-    # plot_data(points[edge], size=40)
-
-# plt.show()
-
-
-
-
-# def gabriel_graph(points : List) -> List:
 
 
 # %%
